810c853cd63c8534dec00669d4925dc3e056a860
adityakp15 <aditponnu@gmail.com>
2021-04-19 17:17:49 +0530

    added changes

--- app.js
+++ app.js
@@ -29,3 +29,8 @@ const User = new mongoose.model("User",userSchema);
 app.get("/",function(req,res){
     res.render("login");
   });
+
+app.listen(3000,function(){
+console.log("Server started on port 3000.");
+});
+  
